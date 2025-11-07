5969ca59b440ad63a1688f184e1fcfd8948b5660
lookshines <lookshinesy2k@yahoo.com>
2025-11-07 08:18:31 +0100

    updated admin url

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("secure-123098/", admin.site.urls),
     path("", include("myapp.urls")),
 ]
